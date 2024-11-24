d19f56df780f674f32fe19223fb8a53b03145bfa
User Name <user@example.com>
2024-11-23 22:15:31 -0500

    wip

--- src/portfolio-web/src/api/axios.ts
+++ src/portfolio-web/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5262', // Replace with your API base URL
+  baseURL: 'https://localhost:5262', // Replace with your API base URL
   headers: {
     'Content-Type': 'application/json',
   },
