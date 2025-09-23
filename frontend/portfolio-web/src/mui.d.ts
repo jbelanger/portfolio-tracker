ac98d3d617e78533c7a997b6252215ae84d8fb9f
jbelanger <your-username@users.noreply.github.com>
2025-09-22 21:21:04 -0400

    Removed portfolio.web frontend project.

--- frontend/portfolio-web/src/mui.d.ts
+++
@@ -1,9 +0,0 @@
-// src/mui.d.ts
-import { ButtonPropsVariantOverrides } from '@mui/material/Button';
-
-declare module '@mui/material/Button' {
-  interface ButtonPropsVariantOverrides {
-    ghost: true;
-    flat: true;
-  }
-}
